423445ec3404bd77de39aec2c0ca5bfe93f60290
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2024-10-28 12:08:09 +0100

    More cleanup

--- src/WLP.hs
+++ src/WLP.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module WLP (makeWLPs, prunedCalcWLP) where
+module WLP (prunedCalcWLP) where
 
-import Expr ( Expr (..), ExprF(..), BinOp(..), Type, prettyishPrintExpr )
+import Expr ( Expr (..), ExprF(..), BinOp(..), Type )
 import Statement ( ExecStmt(..), ExecTree(..), ExecTreeF(..) )
 
 import Data.Functor.Foldable ( Recursive (cata), Corecursive (embed) )
@@ -16,19 +16,15 @@ import Data.DList (singleton)
 import Data.Bool (bool)
 import Cli (ArgData(dumpConditions))
 
--- | Create a list of all WLP's of a program, one for each path (lazily).
-makeWLPs :: Expr -> ExecTree -> [Expr]
-makeWLPs q = cata f
-  where
-    f (NodeF s ts)     = map (wlpStmt s) (concat ts)
-    f (TerminationF s) = [wlpStmt s q]
-
 prunedCalcWLP :: (MonadZ3 m, MonadG m) => Int -> ExecTree -> m Bool
 prunedCalcWLP prune tree = cata f tree [] id 0
   where
     f (NodeF (EAssume e) r) as em d = do
-      ast <- expr2ast (em e)
+      let e' = em e
+      ast <- expr2ast e'
       let next = map (\g -> g (ast : as) em (d + 1)) r
+      whenRs (dumpConditions . options) $ do
+        tell (singleton $ show e')
       if prune <= d then do testChildren next else do
         _res <- checkAssumptions (ast : as)
         case _res of
@@ -41,9 +37,12 @@ prunedCalcWLP prune tree = cata f tree [] id 0
     f (NodeF s r) as em d = let next = map (\g -> g as (em . wlpStmt s) (d + 1)) r
                             in testChildren next
     f (TerminationF s) as em _ = do
-      incrNumPaths     
-      local $ do                                                                                                   
-        ast <- mkNot =<< expr2ast (em (wlpStmt s $ LitB True))
+      incrNumPaths
+      local $ do
+        let e' = em (wlpStmt s $ LitB True)                                                                                      
+        ast <- mkNot =<< expr2ast e'
+        whenRs (dumpConditions . options) $ do
+          tell (singleton $ show e')
         assert =<< mkAnd (ast : as)
         (_res, model) <- getModel
         case model of
@@ -61,14 +60,13 @@ testChildren (mb : mbs) = bool (return False) (testChildren mbs) =<< mb
 
 wlpStmt :: ExecStmt -> Expr -> Expr
 wlpStmt ESkip             = id
--- wlpStmt (EAssert (LitB True)) = id -- Potential optimisation.
 wlpStmt (EAssert e1)      = BinopExpr And e1
 wlpStmt (EAssume e1)      = BinopExpr Implication e1
 wlpStmt (EAssign s e)     = replace s e
 wlpStmt (EAAssign s i e)  = cata f
   where
     f :: ExprF Expr -> Expr
-    f (VarF s' t) = replaceVar s' (RepBy (Var s t) i e) s t--foldExpr (defaultAlgebra {var=replaceVar s e})
+    f (VarF s' t) = replaceVar s' (RepBy (Var s t) i e) s t
     f e' = embed e'
 wlpStmt (EDrefAssign _ _) = optionalError -- Reference types.
 -- wlpStmt EBlock = error "TODO"
